2c11728a9eaa8d488aaf9204566a94abae823e71
Ryan Pavlik <ryan@sensics.com>
2017-11-08 15:55:22 -0600

    Add clang-format

---
+++ format-project.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Takes each line as being a filename, turns it into a null-delimited string
+# and hands it to xargs -0 to pass to clang-format efficiently (because running
+# clang-format once for each file is very slow)
+run_clang_format_on_input_lines() {
+    (while read fn; do
+        printf "%s\0" "$fn"
+    done) | xargs -0 clang-format -style=file -i
+}
+
+(
+cd $(dirname $0)
+
+(
+    cd include
+    ls *.h | run_clang_format_on_input_lines
+)
+
+(
+    cd src
+    ls *.cpp | run_clang_format_on_input_lines
+)
+
+)
